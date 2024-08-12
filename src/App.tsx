5d3f1927fd57f36c9d215424b50bc0ab590d55ac
Sergio <sergiocarrascal000@gmail.com>
2024-08-12 22:44:10 +0200

    added reasons modal

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,24 @@ interface TimeElapsed {
 }
 
 const achievements = [
-  { time: 60 * 60 * 1000, message: "¡He alcanzado 1 hora sin fumar!" },
   { time: 24 * 60 * 60 * 1000, message: "¡He alcanzado 1 día sin fumar!" },
   { time: 2 * 24 * 60 * 60 * 1000, message: "¡He alcanzado 2 días sin fumar!" },
   { time: 3 * 24 * 60 * 60 * 1000, message: "¡He alcanzado 3 días sin fumar!" },
   { time: 4 * 24 * 60 * 60 * 1000, message: "¡He alcanzado 4 días sin fumar!" },
   { time: 5 * 24 * 60 * 60 * 1000, message: "¡He alcanzado 5 días sin fumar!" },
   { time: 6 * 24 * 60 * 60 * 1000, message: "¡He alcanzado 6 días sin fumar!" },
-  { time: 7 * 24 * 60 * 60 * 1000, message: "¡He alcanzado 1 semana sin fumar!" },
-  { time: 14 * 24 * 60 * 60 * 1000, message: "¡He alcanzado 2 semanas sin fumar!" },
-  { time: 21 * 24 * 60 * 60 * 1000, message: "¡He alcanzado 3 semanas sin fumar!" },
+  {
+    time: 7 * 24 * 60 * 60 * 1000,
+    message: "¡He alcanzado 1 semana sin fumar!",
+  },
+  {
+    time: 14 * 24 * 60 * 60 * 1000,
+    message: "¡He alcanzado 2 semanas sin fumar!",
+  },
+  {
+    time: 21 * 24 * 60 * 60 * 1000,
+    message: "¡He alcanzado 3 semanas sin fumar!",
+  },
   { time: 30 * 24 * 60 * 60 * 1000, message: "¡He alcanzado 1 mes sin fumar!" },
   // Añade más logros según sea necesario
 ];
@@ -30,6 +38,7 @@ function App() {
   });
 
   const [achieved, setAchieved] = useState<string[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     const quitDate = new Date();
@@ -48,7 +57,10 @@ function App() {
 
       // Verifica logros
       achievements.forEach((achievement) => {
-        if (difference >= achievement.time && !achieved.includes(achievement.message)) {
+        if (
+          difference >= achievement.time &&
+          !achieved.includes(achievement.message)
+        ) {
           setAchieved((prev) => [...prev, achievement.message]);
         }
       });
@@ -68,6 +80,19 @@ function App() {
           Han pasado {timeElapsed.days} días, {timeElapsed.hours} horas y{" "}
           {timeElapsed.minutes} minutos.
         </h2>
+        <br />
+        <button onClick={() => setIsModalOpen(true)}>Motivos</button>
+        {isModalOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={() => setIsModalOpen(false)}>
+                &times;
+              </span>
+              <h2>Motivos por los cuales dejar de fumar</h2>
+              <p>Aquí puedes listar los motivos...</p>
+            </div>
+          </div>
+        )}
         <br />
         <div className="achievements">
           <h3>Logros conseguidos:</h3>
